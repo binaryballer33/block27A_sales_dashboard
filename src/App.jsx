2a99bb2ea1865ec3f0e803d30c1c1bac1dcfb798
Shaquille Mandy <shaqmandy@gmail.com>
2024-01-09 14:16:55 -0300

    initial project commit
    adding starter files and css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="container">
+            <div className="box">
+                <h1 style={{color: 'lightblue'}}>Sales Dashboard</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
